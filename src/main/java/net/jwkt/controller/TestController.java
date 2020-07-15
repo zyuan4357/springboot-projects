04c6ae036c1ccf49244cbc2656d9363b42845d2a
zyuan4357 <zyuan4357@163.com>
2020-07-15 11:10:30 +0800

    second commit

---
+++ src/main/java/net/jwkt/controller/TestController.java
@@ -0,0 +1,10 @@
+package net.jwkt.controller;
+
+/**
+ * Created by Tiler on 2020/7/15
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("OK OK");
+    }
+}
